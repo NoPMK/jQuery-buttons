7a9c038341904ba504b86c3c631eae7e2c967f52
NoPMK <gacsdan@gmail.com>
2020-06-10 20:47:13 +0200

    hhh

--- script.js
+++ script.js
@@ -4,10 +4,35 @@ $(".first").click(() => {
     console.log("Yeah, you clicked me")
 });
 
+
 $(".second").click(() => {
     $(".first").text("Haha, I got you!")
 });
 
-$(".third").click(() => {
-    $("button").css("background-color" , "red")
+
+
+
+let color_list = ["lime", "yellow", "red", "orange", "aqua", "purple"];
+let i = 0;
+$(".third").click( () => {
+    color = color_list[i];
+    $("button").css("background-color", color);
+    if (i === color_list.length - 1) {
+        i = 0;
+    } else {i = i+1;}
+});
+
+let clicked = true;
+$("third").click( () => {
+    color = $("#color_input").val();
+    if (color === "") {
+        color = "blue"
+    };
+    if (clicked === true) {
+        $("button").css("background-color", color);
+        console.log("it is true")
+        clicked = false;
+    } else {
+        console.log("it is false")
+    };
 });
